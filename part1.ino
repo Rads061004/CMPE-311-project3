505ff1b7e19937a81697f1a38ceb1ed5182a3219
Rads061004 <armaanm1@umbc.edu>
2025-12-02 18:14:05 -0500

    Refactor PWM control and task management

--- part1.ino
+++ part1.ino
@@ -3,47 +3,70 @@
 #define PWM_PIN 9
 #define BUTTON_PIN 4
 
-const int NUM_LEVELS = 6;  
-int dutyLevels[NUM_LEVELS] = {0, 51, 102, 153, 204, 255};  
+const int NUM_LEVELS = 6;
+int dutyLevels[NUM_LEVELS] = {0, 51, 102, 153, 204, 255};
 int currentLevel = 0;
 
-bool lastButton = HIGH;  
+bool lastButton = HIGH;
 unsigned long lastDebounce = 0;
 
-void ledTask() {
-  analogWrite(PWM_PIN, dutyLevels[currentLevel]);
-}
+volatile int pwmCounter = 0;
+volatile int pwmDuty = 0;
 
-void buttonTask() {
-  bool reading = digitalRead(BUTTON_PIN);
-  
-  if (reading == LOW && lastButton == HIGH && (millis() - lastDebounce) > 50) {
-    currentLevel = (currentLevel + 1) % NUM_LEVELS;
-
-    Serial.print("Brightness level: ");
-    Serial.print((currentLevel * 100) / (NUM_LEVELS - 1));
-    Serial.println("%");
-
-    lastDebounce = millis();
-  }
-
-  lastButton = reading;
-}
+void ledTask();
+void buttonTask();
+void pwmTask();
 
 typedef void (*TaskFn)();
-TaskFn tasks[] = { ledTask, buttonTask };
-const int NUM_TASKS = 2;
+TaskFn tasks[] = { ledTask, buttonTask, pwmTask };
+const int NUM_TASKS = 3;
+
+void setupTimer1() {
+  cli();
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCCR1B |= (1 << WGM12);
+  OCR1A = 1999;
+  TIMSK1 |= (1 << OCIE1A);
+  TCCR1B |= (1 << CS10);
+  sei();
+}
+
+ISR(TIMER1_COMPA_vect) {
+  pwmCounter++;
+  if (pwmCounter < pwmDuty) digitalWrite(PWM_PIN, HIGH);
+  else digitalWrite(PWM_PIN, LOW);
+  if (pwmCounter >= 255) pwmCounter = 0;
+}
 
 void setup() {
   Serial.begin(9600);
   pinMode(PWM_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
-
+  setupTimer1();
   Serial.println("System Ready. Press button to change brightness.");
 }
 
 void loop() {
-  for (int i = 0; i < NUM_TASKS; i++) {
-    tasks[i]();
-  }
+  for (int i = 0; i < NUM_TASKS; i++) tasks[i]();
 }
+
+void ledTask() {
+  pwmDuty = dutyLevels[currentLevel];
+}
+
+void buttonTask() {
+  bool reading = digitalRead(BUTTON_PIN);
+  if (reading == LOW && lastButton == HIGH && (millis() - lastDebounce) > 50) {
+    currentLevel = (currentLevel + 1) % NUM_LEVELS;
+    Serial.print("Brightness level: ");
+    Serial.print((currentLevel * 100) / (NUM_LEVELS - 1));
+    Serial.println("%");
+    lastDebounce = millis();
+  }
+  lastButton = reading;
+}
+
+void pwmTask() {
+}
+
